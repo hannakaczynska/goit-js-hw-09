14afc2c7d2c3be9207b60c2838fa2e82485bfff2
hannakaczynska <hannakacz13@gmail.com>
2023-03-09 11:48:33 +0100

    added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let changeBodyColor;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+  changeBodyColor = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+});
+
+buttonStop.addEventListener('click', e => {
+  clearInterval(changeBodyColor);
+  buttonStop.disabled = true;
+  buttonStart.disabled = false;
+});
