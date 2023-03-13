bc3ead792b7a36b3c8be98cf2e41c1446ab2f606
hannakaczynska <hannakacz13@gmail.com>
2023-03-13 15:38:01 +0100

    added

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,12 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const startButton = document.querySelector('[data-start]');
 const dataDays = document.querySelector('[data-days]');
 const dataHours = document.querySelector('[data-hours]');
 const dataMinutes = document.querySelector('[data-minutes]');
 const dataSeconds = document.querySelector('[data-seconds]');
-let actualDate = new Date();
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -30,19 +30,17 @@ function convertMs(ms) {
 const options = {
   enableTime: true,
   time_24hr: true,
-  defaultDate: actualDate,
+  defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] <= actualDate) {
+    if (selectedDates[0] <= new Date()) {
       startButton.disabled = true;
       window.alert('Please choose a date in the future');
     } else {
       startButton.disabled = false;
       const onClick = () => {
         startButton.disabled = true;
-        const msTime = selectedDates[0].getTime() - actualDate.getTime();
-        const startTimerValue = convertMs(msTime);
-        setClock(startTimerValue);
+        updateClock(selectedDates[0]);
       };
       startButton.addEventListener('click', onClick);
     }
@@ -52,22 +50,27 @@ const options = {
 const calendar = flatpickr('#datetime-picker', options);
 
 function setClock({ days, hours, minutes, seconds }) {
-  dataDays.innerText = days;
-  dataHours.innerText = hours;
-  dataMinutes.innerText = minutes;
-  dataSeconds.innerText = seconds;
+  dataDays.innerText = addLeadingZero(days);
+  dataHours.innerText = addLeadingZero(hours);
+  dataMinutes.innerText = addLeadingZero(minutes);
+  dataSeconds.innerText = addLeadingZero(seconds);
 }
 
-function startClock() {
-  let updateClock = setInterval(() => {
-    for (let i = 0; i < msTime; i++) {
-      if (countTime > 0) {
-        let countTime = msTime - 1;
-        const timerValue = convertMs(countTime);
-        setClock(timerValue);
-      } else if (countTime === 0) {
-        break;
-      }
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateClock(dateYouPicked) {
+  setInterval(() => {
+    let msTime = dateYouPicked.getTime() - Date.now();
+    if (msTime > 0) {
+      let timerValue = convertMs(msTime);
+      console.log(timerValue);
+      setClock(timerValue);
+    } else {
+      clearInterval(updateClock);
+      const stopTimer = convertMs(0);
+      setClock(stopTimer);
     }
   }, 1000);
 }
